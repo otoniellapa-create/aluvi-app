8f6b30a461d7c82e0ce40ccfc5acfa4aa8a6ba17
otoniellapa-create <otoniel.lapa@gmail.com>
2026-07-20 16:12:45 -0300

    ALUVI v2.3 - publicacao inicial

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "aluvi-v23";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html"])).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      const rede = fetch(e.request).then(resp => {
+        if (resp && resp.ok) { const cp = resp.clone(); caches.open(CACHE).then(c => c.put(e.request, cp)); }
+        return resp;
+      }).catch(() => hit);
+      return hit || rede;
+    })
+  );
+});
