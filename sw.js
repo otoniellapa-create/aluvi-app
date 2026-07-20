8a1bc0f3c5f95d9c5f49438f596138b810dd911c
otoniellapa-create <otoniel.lapa@gmail.com>
2026-07-20 17:09:06 -0300

    Add files via upload
    ALUVI v3.0 - sincronizacao em nuvem da equipe

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "aluvi-v23";
+const CACHE = "aluvi-v30";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html"])).then(() => self.skipWaiting()));
 });
