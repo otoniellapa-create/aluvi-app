8809b6bad0fed8ca1f3377c0a1b8959c2d865d77
Oto <oto@otoplanejamento.com.br>
2026-07-23 22:31:59 -0300

    Ponto 5: Esqueci minha senha (link no login + envio + tela de nova senha); cache v46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "aluvi-v45";
+const CACHE = "aluvi-v46";
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html"])).then(() => self.skipWaiting()));
